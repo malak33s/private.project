e000a18271364380f11d5d02ac258f7854d39304
lhaniii <ilhan95210@icloud.com>
2024-04-17 01:01:12 +0000

    ajout pid_max et av.c

---
+++ av.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+        int i;
+
+        for (i = 0; av[i] != NULL; i++)
+        {
+                printf("%s ", av[i]);
+        }
+        printf("\n");
+        return (0);
+}
